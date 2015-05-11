101485e60ee62eea7bd64d986e60c1768fe6ad79
Ludovic Roucoux <ludovic.roucoux@gmail.com>
2015-05-11 15:32:11 +0200

    Add file : ZAPreportCollection

---
+++ src/main/java/fr/novia/zaproxyplugin/report/ZAPreportCollection.java
@@ -0,0 +1,71 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 ludovicRoucoux
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package fr.novia.zaproxyplugin.report;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class contains all ZAPreport instance of the application. 
+ * It's a singleton class so the application contains only one instance of the class.
+ * 
+ * @author ludovic.roucoux
+ *
+ */
+public class ZAPreportCollection implements Serializable {
+
+	private static final long serialVersionUID = -470481707608315500L;
+	
+	private static ZAPreportCollection uniqueInstance = new ZAPreportCollection();
+	
+	/** Map where key is the report format represented by a String
+	 *  and value is a ZAPreport object allowing to generate a report with the corresponding format.
+	 */
+	private Map<String, ZAPreport> mapFormatReport;
+
+	private ZAPreportCollection() {
+		mapFormatReport = new HashMap<String, ZAPreport>();
+
+		// ZAPreport's creation
+		ZAPreportXML reportXML = new ZAPreportXML();
+		ZAPreportHTML reportHTML = new ZAPreportHTML();
+		
+		// Add ZAPreport to the map
+		mapFormatReport.put(reportXML.getFormat(), reportXML);
+		mapFormatReport.put(reportHTML.getFormat(), reportHTML);
+	}
+	
+	public static ZAPreportCollection getInstance(){
+		if(uniqueInstance == null)
+			uniqueInstance = new ZAPreportCollection();
+		
+		return uniqueInstance;
+	}
+
+	public Map<String, ZAPreport> getMapFormatReport() {
+		return mapFormatReport;
+	}	
+}
